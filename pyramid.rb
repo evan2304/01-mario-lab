6be86550c165261b2df158b31cf0a8c90c91f56f
Eva Noertoft <evan2304@hstat.org>
2018-10-05 03:25:08 +0000

    add part 2

--- pyramid.rb
+++ pyramid.rb
@@ -1,8 +1,26 @@
 def pyramid 
-    puts "    #  #"
-    puts "   ##  ##"
-    puts "  ###  ###"
-    puts " ####  ####"
-    puts "#####  #####"
+    puts " "*3 + "#"*1 + " "*2 + "#"*1
+    puts " "*2 + "#"*2 + " "*2 + "#"*2
+    puts " "*1 + "#"*3 + " "*2 + "#"*3
+    puts "#"*4 + " "*2 + "#"*4
 end 
 pyramid
+
+def user_height (height)
+    y= height-1
+    z= height-y-1
+    x=0
+    until x== height
+    z+=1
+        puts " "*y + "#"*z + " "*2 + "#"*z + " "*y
+        y-=1
+        x+=1
+    end
+    
+end 
+puts "How tall do you want your pyramid to be?"
+x = gets.chomp.to_i
+user_height(x)
+
+
+
